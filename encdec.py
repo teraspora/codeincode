24cb88df2b0619e5c279d5a4b57c135c91e51c51
John Lynch <johnlynch.32768@gmail.com>
2021-01-24 02:25:01 +0000

    Initial commit.

---
+++ encdec.py
@@ -0,0 +1,25 @@
+# project: codeincode,
+# module: encdec.py,
+# author: John Lynch
+# date: January 2021
+# >>> A playground for encoding and decoding, encrypting and decrypting
+
+from math import ceil
+
+
+def stoi(s):
+    """
+    Encode a string to an integer
+    """
+    return int(''.join(format(ord(ch), 'b').zfill(8) for ch in s), 2)
+
+def itos(n):
+    """
+    Decode an integer into the original string
+    """
+    b = format(n, 'b')
+    bl = ceil(len(b) / 8) * 8
+    b = b.zfill(bl)
+    bsl = ''.join([chr(int(''.join(b[i:i+8]), 2)) for i in range(0, bl, 8)])
+    return bsl
+
