35c208301e40d26914277e79fd0942361837b874
John Lynch <johnlynch.32768@gmail.com>
2021-01-24 02:50:56 +0000

    Add intermediate bitstring converters and provide README.md.

---
+++ README.md
@@ -0,0 +1,15 @@
+# codeincode
+
+## Overview
+
+Codeincode is a playground for encoding and decoding, encrypting and decrypting, converting strings into other forms and back again.
+
+## Initial goals
+
+    - Encode a textual message invisibly in a photograph(jpeg or png file)
+    - Decode the message from the photo
+    - Play with some forms of encryption and password protection
+
+## First steps
+
+    - Create two functions which convert between string and integer; also provide two functions that handle the intermediate bitstring representation, the easiest form for use in pixel processing.
--- encdec.py
+++ encdec.py
@@ -7,6 +7,12 @@
 from math import ceil
 
 
+def stobs(s):
+    """
+    Encode a string to an string of binary digits 0 || 1
+    """
+    return ''.join(format(ord(ch), 'b').zfill(8) for ch in s)
+
 def stoi(s):
     """
     Encode a string to an integer
@@ -23,3 +29,12 @@ def itos(n):
     bsl = ''.join([chr(int(''.join(b[i:i+8]), 2)) for i in range(0, bl, 8)])
     return bsl
 
+
+def bstos(b):
+    """
+    Decode a bitstring into the original string
+    """
+    bl = ceil(len(b) / 8) * 8
+    b = b.zfill(bl)
+    bsl = ''.join([chr(int(''.join(b[i:i+8]), 2)) for i in range(0, bl, 8)])
+    return bsl
